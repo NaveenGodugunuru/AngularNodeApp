1acb2650cae1afc6108743dfedbf59d7a5902867
Naveen <52918091+NaveenGodugunuru@users.noreply.github.com>
2023-10-29 09:30:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ const { request } = require('express');
 app.get('/', callName)
 async function callName(req, res) {
   console.log(req.query.text)
-  // let spawn = require("child_process").spawn;
-  // let process = spawn('python', ["./hello.py", "I am Venkata My annual salary is 80000"]);
-
-  // process.stdout.on('data', function(data) {
-  //   res.send(data.toString());
-  // })
-  try {
+    try {
   //   const data = {
   //     "PERSON": "X",
   //     "AGE": 35,
@@ -33,9 +27,9 @@ async function callName(req, res) {
   //     "RETIREMENT_AMOUNT": 2000,
   //     "LOCATION": "Texas"
   // }
-  // res.send(data)
+  // // res.send(data)
     // const resp = await axios.get('http://127.0.0.1:8010/retirewell/?text='+req.query.text)   
-    const resp = await axios.get('http://3.111.194.62:8000/retirewell/?text='+req.query.text)    
+    const resp = await axios.get('http://52.66.7.28:8000/retirewell/?text='+req.query.text)    
     res.send(resp.data)    
 
   } catch (error) {
@@ -46,22 +40,4 @@ async function callName(req, res) {
 app.listen(app.get('port'), async function() {
   console.log("Node app is running at localhost:" + app.get('port'))
   
-  
-//  https.get('http://localhost:8010/retirewell/?text=60000', (resp) => {
-//   let data = '';
-
-//   // A chunk of data has been received.
-//   resp.on('data', (chunk) => {
-//     data += chunk;
-//   });
-
-//   // The whole response has been received. Print out the result.
-//   resp.on('end', () => {
-//     console.log(JSON.parse(data).explanation);
-//   });
-
-// }).on("error", (err) => {
-//   console.log("Error: " + err.message); 
-// });
-
 })
